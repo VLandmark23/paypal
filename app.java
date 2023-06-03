f39ec13a6e0855376c53093e324eea94773d3127
vbatesLM <vbates.landmark@gmail.com>
2023-06-03 12:57:53 -0400

    modified

--- app.java
+++ app.java
@@ -1,7 +1,18 @@
+<<<<<<< HEAD
 We support;
 java applications, 
 .net applications,
-NodeJS applications,
+NodeJS applica:Qtions,
 python applications,
 web applications,
 bug removed
+We support
+java applications
+.net applications
+NodeJS applications
+python applications
+web application
+great
+This app is excellent
+This app is excellent for all users
+>>>>>>> a6b6e28 (excellent job for all)
